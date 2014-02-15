10cfd670ffe317b82a3bacfca7b08737b6b8b840
Michele Marostica <michelemaro@gmail.com>
2014-02-15 12:27:46 +0100

    Added other strategies to build objects from yaml files

--- src/SettingsGenerator/yamlobjectscreationstrategy.cpp
+++ src/SettingsGenerator/yamlobjectscreationstrategy.cpp
@@ -232,7 +232,7 @@ void ORBDetectorCreationStrategy::createFromYAML(const cv::FileStorage &fs,
             {
                 for(size_t d = 0; d < edgeThreshold.size(); d++)
                 {
-                    for(size_t e = 0; e < v.size(); e++)
+                    for(size_t e = 0; e < WTA_K.size(); e++)
                     {
                         for(size_t f = 0; f < scoreType.size(); f++)
                         {
@@ -311,25 +311,193 @@ void BRISKDescriptorCreationStrategy::createFromYAML(const cv::FileStorage &fs,
 void MSERDetectorCreationStrategy::createFromYAML(const cv::FileStorage &fs,
                                                   std::vector<KeyPointDetector::ConstPtr> &vector) const
 {
+    //int _delta, int _min_area, int _max_area,
+    //float _max_variation, float _min_diversity,
+    //int _max_evolution, double _area_threshold,
+    //double _min_margin, int _edge_blur_size
 
+    std::vector<std::string>
+            delta,
+            minArea,
+            maxArea,
+            maxVariation,
+            minVariation,
+            maxEvolution,
+            areaThreshold,
+            minMargin,
+            edgeBlurSize;
+
+    fs["FeatureOptions"]["delta"] >> delta;
+    fs["FeatureOptions"]["minArea"] >> minArea;
+    fs["FeatureOptions"]["maxVariation"] >> maxVariation;
+    fs["FeatureOptions"]["minVariation"] >> minVariation;
+    fs["FeatureOptions"]["maxEvolution"] >> maxEvolution;
+    fs["FeatureOptions"]["areaThreshold"] >> areaThreshold;
+    fs["FeatureOptions"]["minMargin"] >> minMargin;
+    fs["FeatureOptions"]["maxArea"] >> maxArea;
+    fs["FeatureOptions"]["edgeBlurSize"] >> edgeBlurSize;
+
+    for (size_t a = 0; a < delta.size(); a++)
+    {
+        for(size_t b = 0; b < minArea.size(); b++)
+        {
+            for(size_t c = 0; c < maxVariation.size(); c++)
+            {
+                for(size_t d = 0; d < minVariation.size(); d++)
+                {
+                    for(size_t e = 0; e < maxEvolution.size(); e++)
+                    {
+                        for(size_t f = 0; f < areaThreshold.size(); f++)
+                        {
+                            for(size_t g = 0; g < minMargin.size(); g++)
+                            {
+                                for(size_t h = 0; h < maxArea.size(); h++)
+                                {
+                                    for(size_t i = 0; i < edgeBlurSize.size(); i++)
+                                    {
+                                        std::string name = "MSER";
+                                        parameters params;
+
+                                        params.insert(std::pair<param_name, param_value>("delta", delta[a]));
+                                        params.insert(std::pair<param_name, param_value>("minArea", minArea[b]));
+                                        params.insert(std::pair<param_name, param_value>("maxVariation", maxVariation[c]));
+                                        params.insert(std::pair<param_name, param_value>("minVariation", minVariation[d]));
+                                        params.insert(std::pair<param_name, param_value>("maxEvolution", maxEvolution[e]));
+                                        params.insert(std::pair<param_name, param_value>("areaThreshold", areaThreshold[f]));
+                                        params.insert(std::pair<param_name, param_value>("minMargin", minMargin[g]));
+                                        params.insert(std::pair<param_name, param_value>("maxArea", maxArea[h]));
+                                        params.insert(std::pair<param_name, param_value>("edgeBlurSize", edgeBlurSize[i]));
+
+                                        YAMLParameterizedObject obj(name, params);
+
+                                        vector.push_back(std::make_shared<const YAMLParameterizedObject>(obj));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 void GFTTDetectorCreationStrategy::createFromYAML(const cv::FileStorage &fs,
                                                   std::vector<KeyPointDetector::ConstPtr> &vector) const
 {
+    // int maxCorners=1000, double qualityLevel=0.01,
+    // double minDistance=1., int blockSize=3,
+    // bool useHarrisDetector=false, double k=0.04
 
+
+    std::vector<std::string>
+            maxCorners,
+            qualityLevel,
+            minDistance,
+            blockSize,
+            useHarrisDetector,
+            k;
+
+    fs["FeatureOptions"]["maxCorners"] >> maxCorners;
+    fs["FeatureOptions"]["qualityLevel"] >> qualityLevel;
+    fs["FeatureOptions"]["minDistance"] >> minDistance;
+    fs["FeatureOptions"]["blockSize"] >> blockSize;
+    fs["FeatureOptions"]["useHarrisDetector"] >> useHarrisDetector;
+    fs["FeatureOptions"]["k"] >> k;
+
+    for (size_t a = 0; a < maxCorners.size(); a++)
+    {
+        for(size_t b = 0; b < qualityLevel.size(); b++)
+        {
+            for(size_t c = 0; c < minDistance.size(); c++)
+            {
+                for(size_t d = 0; d < blockSize.size(); d++)
+                {
+                    for(size_t e = 0; e < useHarrisDetector.size(); e++)
+                    {
+                        for(size_t f = 0; f < k.size(); f++)
+                        {
+                            std::string name = "GFTT";
+                            parameters params;
+
+                            params.insert(std::pair<param_name, param_value>("maxCorners", maxCorners[a]));
+                            params.insert(std::pair<param_name, param_value>("qualityLevel", qualityLevel[b]));
+                            params.insert(std::pair<param_name, param_value>("minDistance", minDistance[c]));
+                            params.insert(std::pair<param_name, param_value>("blockSize", blockSize[d]));
+                            params.insert(std::pair<param_name, param_value>("useHarrisDetector", useHarrisDetector[e]));
+                            params.insert(std::pair<param_name, param_value>("k", k[f]));
+                            YAMLParameterizedObject obj(name, params);
+
+                            vector.push_back(std::make_shared<const YAMLParameterizedObject>(obj));
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 void FREAKDescriptorCreationStrategy::createFromYAML(const cv::FileStorage &fs,
                                                      std::vector<DescriptorExtractor::ConstPtr> &vector) const
 {
+    //bool orientationNormalized=true, bool scaleNormalized=true, float patternScale=22.0f, int nOctaves=4
+    std::vector<std::string>
+            orientationNormalized,
+            scaleNormalized,
+            patternScale,
+            nOctaves;
 
+    fs["FeatureOptions"]["orientationNormalized"] >> orientationNormalized;
+    fs["FeatureOptions"]["scaleNormalized"] >> scaleNormalized;
+    fs["FeatureOptions"]["patternScale"] >> patternScale;
+    fs["FeatureOptions"]["nOctaves"] >> nOctaves;
+
+    for (size_t a = 0; a < orientationNormalized.size(); a++)
+    {
+        for(size_t b = 0; b < scaleNormalized.size(); b++)
+        {
+            for(size_t c = 0; c < patternScale.size(); c++)
+            {
+                for(size_t d = 0; d < nOctaves.size(); d++)
+                {
+                    std::string name = "FREAK";
+                    parameters params;
+
+                    params.insert(std::pair<param_name, param_value>("orientationNormalized", orientationNormalized[a]));
+                    params.insert(std::pair<param_name, param_value>("scaleNormalized", scaleNormalized[b]));
+                    params.insert(std::pair<param_name, param_value>("patternScale", patternScale[c]));
+                    params.insert(std::pair<param_name, param_value>("nOctaves", nOctaves[d]));
+
+                    YAMLParameterizedObject obj(name, params);
+
+                    vector.push_back(std::make_shared<const YAMLParameterizedObject>(obj));
+                }
+            }
+        }
+    }
 }
 
 void BRIEFDescriptorCreationStrategy::createFromYAML(const cv::FileStorage &fs,
                                                      std::vector<DescriptorExtractor::ConstPtr> &vector) const
 {
+    // bytes is a length of descriptor in bytes. It can be equal 16, 32 or 64 bytes.
 
+    std::vector<std::string>
+            bytes;
+
+    fs["FeatureOptions"]["bytes"] >> bytes;
+
+    for (size_t a = 0; a < bytes.size(); a++)
+    {
+        std::string name = "BRIEF";
+        parameters params;
+
+        params.insert(std::pair<param_name, param_value>("bytes", bytes[a]));
+
+        YAMLParameterizedObject obj(name, params);
+
+        vector.push_back(std::make_shared<const YAMLParameterizedObject>(obj));
+    }
 }
 
 }
